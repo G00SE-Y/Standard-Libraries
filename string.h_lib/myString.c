8580ac44539f8a3aa6a9744555c7b7add0fd05f8
Ethan <bulldawg0065@gmail.com>
2022-07-20 22:43:16 -0400

    Implemented more functions and tests for those functions.

--- string.h_lib/myString.c
+++ string.h_lib/myString.c
@@ -11,22 +11,22 @@ void *memcpy(void* dest, const void* src, size_t n); // Copies n characters from
 void *memmove(void* dest, const void* src, size_t n); // Another function to copy n characters from str2 to str1.
 void *memset(void* str, int c, size_t n); // Copies the character c (an unsigned char) to the first n characters of the string pointed to, by the argument str.
 char *strcat(char* dest, const char* src); // Appends the string pointed to, by src to the end of the string pointed to by dest.
-char *strncat(char* dest, const char* src, size_t n); // Appends the string pointed to, by src to the end of the string pointed to, by dest up to n characters long.
-char *strchr(const char* str, int c); // Searches for the first occurrence of the character c (an unsigned char) in the string pointed to, by the argument str.
-int strcmp(const char* str1, const char* str2); // Compares the string pointed to, by str1 to the string pointed to by str2.
-int strncmp(const char* str1, const char* str2, size_t n); // Compares at most the first n bytes of str1 and str2.
-int strcoll(const char* str1, const char* str2); // Compares string str1 to str2. The result is dependent on the LC_COLLATE setting of the location.
-char *strcpy(char* dest, const char* src); // Copies the string pointed to, by src to dest.
-char *strncpy(char* dest, const char* src, size_t n); // Copies up to n characters from the string pointed to, by src to dest.
-size_t strcspn(const char* str1, const char* str2); // Calculates the length of the initial segment of str1 which consists entirely of characters not in str2.
-char *strerror(int errnum); // Searches an internal array for the error number errnum and returns a pointer to an error message string.
-size_t strlen(const char* str); // Computes the length of the string str up to but not including the terminating null character.
-char *strpbrk(const char* str1, const char* str2); // Finds the first character in the string str1 that matches any character specified in str2.
-char *strrchr(const char* str, int c); // Searches for the last occurrence of the character c (an unsigned char) in the string pointed to by the argument str.
-size_t strspn(const char* str1, const char* str2); // Calculates the length of the initial segment of str1 which consists entirely of characters in str2.
-char *strstr(const char* haystack, const char* needle); // Finds the first occurrence of the entire string needle (not including the terminating null character) which appears in the string haystack.
-char *strtok(char* str, const char* delim); // Breaks string str into a series of tokens separated by delim.
-size_t strxfrm(char* dest, const char* src, size_t n); // Transforms the first n characters of the string src into current locale and places them in the string dest.
+// char *strncat(char* dest, const char* src, size_t n); // Appends the string pointed to, by src to the end of the string pointed to, by dest up to n characters long.
+// char *strchr(const char* str, int c); // Searches for the first occurrence of the character c (an unsigned char) in the string pointed to, by the argument str.
+// int strcmp(const char* str1, const char* str2); // Compares the string pointed to, by str1 to the string pointed to by str2.
+// int strncmp(const char* str1, const char* str2, size_t n); // Compares at most the first n bytes of str1 and str2.
+// int strcoll(const char* str1, const char* str2); // Compares string str1 to str2. The result is dependent on the LC_COLLATE setting of the location.
+// char *strcpy(char* dest, const char* src); // Copies the string pointed to, by src to dest.
+// char *strncpy(char* dest, const char* src, size_t n); // Copies up to n characters from the string pointed to, by src to dest.
+// size_t strcspn(const char* str1, const char* str2); // Calculates the length of the initial segment of str1 which consists entirely of characters not in str2.
+// char *strerror(int errnum); // Searches an internal array for the error number errnum and returns a pointer to an error message string.
+// size_t strlen(const char* str); // Computes the length of the string str up to but not including the terminating null character.
+// char *strpbrk(const char* str1, const char* str2); // Finds the first character in the string str1 that matches any character specified in str2.
+// char *strrchr(const char* str, int c); // Searches for the last occurrence of the character c (an unsigned char) in the string pointed to by the argument str.
+// size_t strspn(const char* str1, const char* str2); // Calculates the length of the initial segment of str1 which consists entirely of characters in str2.
+// char *strstr(const char* haystack, const char* needle); // Finds the first occurrence of the entire string needle (not including the terminating null character) which appears in the string haystack.
+// char *strtok(char* str, const char* delim); // Breaks string str into a series of tokens separated by delim.
+// size_t strxfrm(char* dest, const char* src, size_t n); // Transforms the first n characters of the string src into current locale and places them in the string dest.
 
 
 // Searches for the first occurrence of the character c (an unsigned char) in the first n bytes of the string pointed to, by the argument str.
@@ -65,14 +65,43 @@ int memcmp(const void *str1, const void *str2, size_t n) {
 
 // Copies n characters from src to dest.
 void *memcpy(void *dest, const void *src, size_t n) {
+    
+    for (size_t i = 0; i < n; i++)
+    {
+        *(char *)(dest + i) = 0x00000000;
+        *(char *)(dest + i) = *(char *)(dest + i) | *(char *)(src + i);
+    }
+
+    return dest;
+}
+
+// Another function to copy n characters from str2 to str1 using an intermediate buffer. (safer than memcpy)
+void *memmove(void *dest, const void *src, size_t n) {
 
     char buf = 0;
 
-    for(size_t i = 0; i < n; i++) {
-        buf = *(char*)(src + i);
-        *(char*)(dest + i) = 0x00000000;
-        *(char*)(dest + i) = *(char*)(dest + i) | buf;
+    for (size_t i = 0; i < n; i++)
+    {
+        buf = *(char *)(src + i);
+        *(char *)(dest + i) = 0x00000000;
+        *(char *)(dest + i) = *(char *)(dest + i) | buf;
     }
+
+    return dest;
+}
+
+// Appends the string pointed to, by src to the end of the string pointed to by dest.
+char *strcat(char *dest, const char *src) {
+
+    size_t i = 0, j = 0;
+
+    while(dest[j] != 0) { j++; }
+
+    for ( ; *(char *)(src + i) != 0; i++, j++) {
+        dest[j] = *(char*)(src + i);
+    }
+
+    dest[j] = 0;
 
     return dest;
 }
--- string.h_lib/myString_Test.c
+++ string.h_lib/myString_Test.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "myString.h"
 
 
@@ -13,24 +14,46 @@ int main() {
     const char alphabet[] = "abcdefghijklmnopqrstuvwxyz\0"; // test string
     const char revalpha[] = "zyxwvutsrqponmlkjihgfedcba\0";
     const char zeros[] = "00000000000000000000000000\0";
+    const char byte69[] = "00100101";
+    const char byte420[] = "11010100";
+    const char byte87[] = "01010111";
+    const char byte25[] = "00011001";
     char test[] = "00000000000000000000000000\0";
 
     // tests for memchr
-    fprintf(f, "%s\n", (char*)memchr(alphabet, 'a', 26));
-    fprintf(f, "%s\n", (char*)memchr(alphabet, 'm', 26));
-    fprintf(f, "%s\n", (char*)memchr(alphabet, 'z', 26));
+    fprintf(f, "\nmemchr\n");
+    fprintf(f, "\t%s\n", (char*)memchr(alphabet, 'a', 26));
+    fprintf(f, "\t%s\n", (char*)memchr(alphabet, 'm', 26));
+    fprintf(f, "\t%s\n", (char*)memchr(alphabet, 'z', 26));
 
     // tests for memcmp
-    fprintf(f, "%d\n", memcmp(alphabet, alphabet, 26));
-    fprintf(f, "%d\n", memcmp(alphabet, zeros, 26));
-    fprintf(f, "%d\n", memcmp(alphabet, revalpha, 26));
-    fprintf(f, "%d\n", memcmp(alphabet, "a", 26));
-    fprintf(f, "%d\n", memcmp("z", revalpha, 26));
+    fprintf(f, "\nmemcmp\n");
+    fprintf(f, "\t%d\n", memcmp(alphabet, alphabet, 26));
+    fprintf(f, "\t%d\n", memcmp(alphabet, zeros, 26));
+    fprintf(f, "\t%d\n", memcmp(alphabet, revalpha, 26));
+    fprintf(f, "\t%d\n", memcmp(alphabet, "a", 26));
+    fprintf(f, "\t%d\n", memcmp("z", revalpha, 26));
 
     // tests for memcpy
-    fprintf(f, "%s\n", (char *)memcpy(test, alphabet, 26));
-    fprintf(f, "%s\n", (char *)memcpy(test, revalpha, 26));
-    fprintf(f, "%s\n", (char *)memcpy(test, zeros, 26));
+    fprintf(f, "\nmemchrcpy\n");
+    fprintf(f, "\t%s\n", (char *)memcpy(test, alphabet, 26));
+    fprintf(f, "\t%s\n", (char *)memcpy(test, revalpha, 26));
+    fprintf(f, "\t%s\n", (char *)memcpy(test, zeros, 26));
+
+    // tests for memmove
+    fprintf(f, "\nmemmove\n");
+    fprintf(f, "\t%s\n", (char *)memmove(test, alphabet, 26));
+    fprintf(f, "\t%s\n", (char *)memmove(test, revalpha, 26));
+    fprintf(f, "\t%s\n", (char *)memmove(test, zeros, 26));
+
+    // tests for strcat
+    char* dma = (char*)malloc((4 * 8) + 1);
+    dma[0] = 0;
+    fprintf(f, "\nstrcat\n");
+    fprintf(f, "\t%s\n", dma = (char *)strcpy(dma, byte69));
+    fprintf(f, "\t%s\n", dma = (char *)strcpy(dma, byte420));
+    fprintf(f, "\t%s\n", dma = (char *)strcpy(dma, byte87));
+    fprintf(f, "\t%s\n", dma = (char *)strcpy(dma, byte25));
 
     fclose(f);
     return 0;
